61307df46946f3ceafc220b8be9f868130ce92ea
ckorir <chrisruto.k@gmail.com>
2023-11-10 14:07:48 +0300

    Added code for showing on watchlist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import CurrencyCollection from './components/CurrencyCollection';
+import WatchList from './components/WatchList';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <CurrencyCollection />
+      <WatchList />
     </div>
   );
 }
--- src/components/WatchList.js
+++ src/components/WatchList.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function WatchList({ currency, releaseFromYourWatchlist, dischargeCoin }) {
+export default function WatchList({ list, releaseFromYourWatchlist, dischargeCoin }) {
   return (
-    <div className="row mx-auto myArmy">
-      {currency && currency.map(coin => (
+    <div className="row mx-auto myList">
+      {list && list.map(coin => (
         <div className='col-md-3' key={coin.id}>
           <div className='cards my-2'>
             <div className='col coininfo p-3'>
