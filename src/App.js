9ac4121d6e9849806e172f36a668fb795c2282af
ckorir <chrisruto.k@gmail.com>
2023-11-10 14:58:48 +0300

    Added code for footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import CurrencyCollection from './components/CurrencyCollection';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-     
       <CurrencyCollection />
+      <Footer />
       
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <p className='name'>Made by Chris Ruto</p>
+        <p>Currency Watch</p>
+    </div>
+  )
+}
