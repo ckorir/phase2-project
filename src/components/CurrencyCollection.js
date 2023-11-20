d4637a80c3d9d356d89a06a04e4265bf18e38d2b
ckorir <chrisruto.k@gmail.com>
2023-11-20 09:22:09 +0300

    Fixed url

--- src/components/CurrencyCollection.js
+++ src/components/CurrencyCollection.js
@@ -13,7 +13,7 @@ export default function CurrencyCollection() {
     }, []);
 
     function fetchData() {
-        const fetchurl = 'http://localhost:3000/total_market_cap';
+        const fetchurl = 'https://phase2-project-ten.vercel.app/total_market_cap';
     
         fetch(fetchurl)
           .then((response) => response.json())
